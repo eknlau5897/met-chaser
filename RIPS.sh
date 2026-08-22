4aca773202a9585052ef5b0c82899697e92e7e3d
eknlau5897 <eknlau@gmail.com>
2026-08-22 09:21:04 +0800

    update

--- RIPS.sh
+++ RIPS.sh
@@ -25,7 +25,7 @@ while true; do
     if [ -z "$LATEST_FILE" ]; then
         echo "[$(date -u)] Directory index restricted. Calculating expected 6-hourly cycle URL..."
         # Calculate latest synoptic hour (00, 06, 12, 18 UTC)
-        CURRENT_HOUR=$((10#$CURRENT_HOUR - 6))
+        CURRENT_HOUR=$(($CURRENT_HOUR - 6))
         SYNOP_HOUR=$(printf "%02d" $(( (10#$CURRENT_HOUR / 6) * 6 )))
         DATE_STAMP=$(date -u +"%Y%m%d")${SYNOP_HOUR}00
         LATEST_FILE="${STORM_ID}_ripastbl_${DATE_STAMP}.txt"
